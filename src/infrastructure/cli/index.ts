9b6ed3b234b69d6dd1f86e1bd0080c2aaa633ec3
Ian <ian01story@gmail.com>
2023-02-28 21:19:49 +0100

    feat: add architecture and get pokemon type use case

---
+++ src/infrastructure/cli/index.ts
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/no-floating-promises */
+/* eslint-disable @typescript-eslint/require-await */
+/* eslint-disable @typescript-eslint/no-require-imports */
+/* eslint-disable @typescript-eslint/no-var-requires */
+const createCLIProgram = require("./cli");
+
+const main = async () => {
+	const program = createCLIProgram();
+	program.parseAsync(process.argv);
+};
+
+main();
