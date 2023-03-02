27f64633b07d746683245e5ab404076dd3a1a1ad
pau7rr <pau.rosquellas@gmail.com>
2023-03-02 21:03:26 +0100

    fix: undefined when pokemon_name didn't exist

--- src/infrastructure/apirest/routes/routes.ts
+++ src/infrastructure/apirest/routes/routes.ts
@@ -4,7 +4,7 @@ import PokemonRepository from "../../repositories/pokemon.repository";
 
 export const loadApiEndpoints = (app: Application): void => {
 	app.get("/type", (req: Request, res: Response) => {
-        const pokemonName = req.query.pokemon_name as string
+        const pokemonName = req?.query?.pokemon_name as string
         const getPokemonTypesUseCase = new GetPokemonTypesUseCase(new PokemonRepository())
 			
 			getPokemonTypesUseCase.execute(pokemonName).then((types) => {
