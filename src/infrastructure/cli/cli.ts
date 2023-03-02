a3b3c51a27c2c06ff15c7b799331acd5831c4476
pau7rr <pau.rosquellas@gmail.com>
2023-03-02 21:19:28 +0100

    fix: linter fixed

--- src/infrastructure/cli/cli.ts
+++ src/infrastructure/cli/cli.ts
@@ -1,27 +1,26 @@
-import { Command } from "commander";
-import GetPokemonTypesUseCase from "../../application/use-cases/get-pokemon-types.use-case";
-import PokemonRepository from "../repositories/pokemon.repository";
-
+import { Command } from 'commander';
+import GetPokemonTypesUseCase from '../../application/use-cases/get-pokemon-types.use-case';
+import PokemonRepository from '../repositories/pokemon.repository';
 
 const CLI = () => {
-	const program = new Command();
-	program.version("1.0.0");
-	program.name("Pokemon Types");
-	program.description("Command line tool to perform Pokemon operations.");
+  const program = new Command();
+  program.version('1.0.0');
+  program.name('Pokemon Types');
+  program.description('Command line tool to perform Pokemon operations.');
 
-	program
-		.command("types")
-		.requiredOption("-n, --name <name>", "Pokemon name")
-		.description("Return Pokemon types")
-		.action((options) => {
-			const getPokemonTypesUseCase = new GetPokemonTypesUseCase(new PokemonRepository())
-			
-			getPokemonTypesUseCase.execute(options.name).then((types) => {
-				console.log(types);
-			});
-		});
+  program
+    .command('types')
+    .requiredOption('-n, --name <name>', 'Pokemon name')
+    .description('Return Pokemon types')
+    .action((options) => {
+      const getPokemonTypesUseCase = new GetPokemonTypesUseCase(new PokemonRepository());
 
-	return program;
+      getPokemonTypesUseCase.execute(options.name).then((types) => {
+        console.log(types);
+      });
+    });
+
+  return program;
 };
 
 module.exports = CLI;
