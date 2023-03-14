be98a9cb270fccdd7f777a6497a8a030e01baf72
santiagoibarzabal <santiagoibarzabal91@gmail.com>
2023-03-14 21:22:56 +0100

    feat: add users use cases tests

---
+++ tests/poke-dex/users/application/use-cases/create-user.use-case.spec.ts
@@ -0,0 +1,28 @@
+import CreateUserUseCase from "../../../../../src/poke-dex/users/application/use-cases/create-user.use-case";
+import InMemoryUserRepository from "../../../../../src/poke-dex/users/infrastructure/repository/in-memory-user.repository";
+import UserCreatorService from "../../../../../src/poke-dex/users/domain/services/user-creator.service";
+import CreateUserDto from "../../../../../src/poke-dex/users/application/dtos/create-user.dto";
+
+jest.mock("../../../../../src/poke-dex/users/application/dtos/create-user.dto");
+const MockDto = CreateUserDto as jest.MockedClass<typeof CreateUserDto>
+
+jest.mock("../../../../../src/poke-dex/users/domain/services/user-creator.service");
+const MockService = UserCreatorService as jest.MockedClass<typeof UserCreatorService>
+
+jest.mock("../../../../../src/poke-dex/users/infrastructure/repository/in-memory-user.repository");
+const MockUserRepository = InMemoryUserRepository as jest.MockedClass<typeof InMemoryUserRepository>
+
+
+describe('CreateUserUseCase', () => {
+    it('should be defined', () => {
+        expect(CreateUserUseCase).toBeDefined();
+    });
+
+    it('should be executed', () => {
+        const dto = new MockDto();
+        dto.name = 'Santiago';
+        dto.id = 1;
+        const useCase = new CreateUserUseCase(new MockUserRepository());
+        useCase.execute(dto);
+    });
+});
