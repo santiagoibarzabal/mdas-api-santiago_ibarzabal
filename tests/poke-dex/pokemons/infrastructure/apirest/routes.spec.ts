88b47e284f5d5f77575431500ea235da93636fbf
santiagoibarzabal <santiagoibarzabal91@gmail.com>
2023-03-23 22:44:27 +0100

    feat: add pokemon detail acceptance test

--- tests/poke-dex/pokemons/infrastructure/apirest/routes.spec.ts
+++ tests/poke-dex/pokemons/infrastructure/apirest/routes.spec.ts
@@ -1,5 +1,5 @@
 import request from "supertest";
-import { app } from "../../../../../src/poke-dex/app";
+import { app } from  "../../../../../src/poke-dex/app";
 
 describe("GET /type", () => {
   it("should return 200 OK with a pokemon name as param", async () => {
@@ -15,6 +15,18 @@ describe("GET /type", () => {
 
   it("should return 404 NOT FOUND with a pokemon name as param", async () => {
     const response = await request(app).get("/type?pokemon_name=invalid");
+    expect(response.status).toBe(404);
+  });
+});
+
+describe("GET /pokemons/:id", () => {
+  it("should return 200 OK with a pokemon id as param", async () => {
+    const response = await request(app).get("/pokemon/1");
+    expect(response.status).toBe(200);
+  });
+
+  it("should return 404 NOT FOUND with an invalid pokemon id as param", async () => {
+    const response = await request(app).get("/pokemon/123123123123");
     expect(response.status).toBe(404);
   });
 });
