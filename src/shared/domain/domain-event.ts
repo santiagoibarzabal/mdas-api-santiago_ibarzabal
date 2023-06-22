3a1bc92039b27c95b9fe0539e31304d1c756791d
santiagoibarzabal <santiagoibarzabal91@gmail.com>
2023-06-22 22:18:51 +0200

    fix: add type public method to abstract domain event

--- src/shared/domain/domain-event.ts
+++ src/shared/domain/domain-event.ts
@@ -3,7 +3,11 @@ import DomainEventMetadata from "./domain-event-metadata";
 export default abstract class DomainEvent {
     private aggregateId: string;
     private metadata: DomainEventMetadata
-    private type: string;
+    private readonly type: string;
+
+    public eventType(): string {
+        return this.type;
+    }
 
     protected constructor(
         aggregateId: string,
