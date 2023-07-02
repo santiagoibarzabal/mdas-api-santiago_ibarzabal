5d075667c6df3435585e5b5f7247a9b5e02e85c8
santiagoibarzabal <santiagoibarzabal91@gmail.com>
2023-07-02 22:07:52 +0200

    feat: add times selected as favorite to pokemon aggregate

--- src/poke-dex/pokemons/domain/pokemon.aggregate.ts
+++ src/poke-dex/pokemons/domain/pokemon.aggregate.ts
@@ -1,5 +1,6 @@
 import PokemonType from "./entities/pokemon-type.entity"
 import { PokemonId, PokemonName, PokemonHeight, PokemonWeight } from "./value-objects";
+import PokemonTimesSelectedAsFavoriteCount from "./value-objects/pokemon-times-selected-as-favorite-count";
 
 
 class PokemonAggregate {
@@ -8,13 +9,22 @@ class PokemonAggregate {
   private readonly types: PokemonType[];
   private readonly height: PokemonHeight;
   private readonly weight: PokemonWeight;
+  private readonly timesSelectedAsFavorite: PokemonTimesSelectedAsFavoriteCount
 
-  constructor(id: PokemonId, name: PokemonName, types: PokemonType[], height: PokemonHeight, weight: PokemonWeight) {
-    this.id = id;
-    this.name = name;
-    this.types = types;
-    this.height = height;
-    this.weight = weight;
+  constructor(
+    id: PokemonId,
+    name: PokemonName,
+    types: PokemonType[],
+    height: PokemonHeight,
+    weight: PokemonWeight,
+    timesAddedAsFavorite: PokemonTimesSelectedAsFavoriteCount = new PokemonTimesSelectedAsFavoriteCount(0),
+  ) {
+      this.id = id;
+      this.name = name;
+      this.types = types;
+      this.height = height;
+      this.weight = weight;
+      this.timesSelectedAsFavorite = timesAddedAsFavorite ?? new PokemonTimesSelectedAsFavoriteCount(0);
   }
 
   public getId(): PokemonId {
@@ -35,6 +45,14 @@ class PokemonAggregate {
 
   public getWeight(): PokemonWeight {
     return this.weight;
+  }
+
+  public selectedAsFavoriteCount(): PokemonTimesSelectedAsFavoriteCount {
+    return this.timesSelectedAsFavorite;
+  }
+
+  public incrementSelectedAsFavoriteCount(): void {
+    this.timesSelectedAsFavorite.increment();
   }
 
 }
