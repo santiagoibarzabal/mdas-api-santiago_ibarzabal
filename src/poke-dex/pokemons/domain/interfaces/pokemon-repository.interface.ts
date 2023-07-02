df981ed2802815f408c3a5ab2b05f07a06cf415e
santiagoibarzabal <santiagoibarzabal91@gmail.com>
2023-07-02 22:13:26 +0200

    feat: add update to pokemon repo interface

--- src/poke-dex/pokemons/domain/interfaces/pokemon-repository.interface.ts
+++ src/poke-dex/pokemons/domain/interfaces/pokemon-repository.interface.ts
@@ -4,4 +4,5 @@ import { PokemonId, PokemonName } from "../value-objects";
 export interface PokemonRepository {
   getPokemonByName(name: PokemonName): Promise<PokemonAggregate>;
   getPokemonById(id: PokemonId): Promise<PokemonAggregate>;
+  update(PokemonAggregate: PokemonAggregate): void;
 }
