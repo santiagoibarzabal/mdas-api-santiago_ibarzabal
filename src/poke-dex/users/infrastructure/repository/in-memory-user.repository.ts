0380ca458d8d282b820f32f8e468257b146a8a6b
santiagoibarzabal <santiagoibarzabal91@gmail.com>
2023-07-04 09:13:45 +0200

    refactor: use user in memory variable in user repository

--- src/poke-dex/users/infrastructure/repository/in-memory-user.repository.ts
+++ src/poke-dex/users/infrastructure/repository/in-memory-user.repository.ts
@@ -3,16 +3,17 @@ import UserAggregate from "../../domain/user.aggregate";
 import { UserId } from "../../domain/value-objects";
 import { PokemonId } from "../../../pokemons/domain/value-objects";
 import { UserNotFoundException } from "../../domain/exceptions/user-not-found";
+import { users } from "./users";
 
 class InMemoryUserRepository implements UserRepositoryInterface {
-  private users: UserAggregate[] = [];
+  private users: UserAggregate[] = users;
 
   save(user: UserAggregate): void {
     this.users.push(user);
   }
 
   addFavouritePokemon(userId: UserId, pokemonId: PokemonId): UserAggregate {
-    const user = this.users.find((user) => user.getId() === userId);
+    const user = this.users.find((user) => user.getId().value === userId.value);
     if (!user) {
       throw UserNotFoundException;
     }
