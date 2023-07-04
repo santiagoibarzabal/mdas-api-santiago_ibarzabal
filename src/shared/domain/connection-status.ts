f28e3c32d75da0d29fa5a36c7f4e95a7c04973ab
santiagoibarzabal <santiagoibarzabal91@gmail.com>
2023-07-04 19:52:41 +0200

    feat: add connection status enum

--- src/shared/domain/connection-status.ts
+++ src/shared/domain/connection-status.ts
@@ -1,20 +1,4 @@
-import DomainEventMetadata from "./domain-event-metadata";
-
-export default abstract class DomainEvent {
-    protected aggregateId: number;
-    protected metadata: DomainEventMetadata
-    protected type: string;
-
-    public eventType(): string {
-        return this.type;
-    }
-
-    protected constructor(
-        aggregateId: number,
-        type: string,
-    ) {
-        this.aggregateId = aggregateId;
-        this.type = type;
-        this.metadata = new DomainEventMetadata(aggregateId);
-    }
+export enum ConnectionStatus {
+    OK = 0,
+    FAILED,
 }
