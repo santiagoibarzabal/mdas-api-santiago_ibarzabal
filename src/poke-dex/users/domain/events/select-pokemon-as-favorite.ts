c45ddc89fc7b350a54389a0c90bd0649f487e8f0
santiagoibarzabal <santiagoibarzabal91@gmail.com>
2023-07-04 09:14:54 +0200

    refactor: rename event type in select pokemon event

--- src/poke-dex/users/domain/events/select-pokemon-as-favorite.ts
+++ src/poke-dex/users/domain/events/select-pokemon-as-favorite.ts
@@ -1,17 +1,14 @@
 import DomainEvent from "../../../../shared/domain/domain-event";
 
 export default class SelectPokemonAsFavorite extends DomainEvent {
-  private readonly pokemonId: number;
-
-  public getPokemonId(): number {
-    return this.pokemonId;
-  }
+  readonly pokemonId: number;
 
   constructor(
     aggregateId: number,
     pokemonId: number
   ) {
-    super(aggregateId, "poke-dex.users.domain.user.select-pokemon-as-favorite");
+    super(aggregateId, "poke-dex.users.select-pokemon-as-favorite");
     this.pokemonId = pokemonId;
   }
+
 }
